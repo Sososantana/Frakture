7958fe0ceb1395e60ee0010eacfbb847e0af095c
Sososantana <127478910+Sososantana@users.noreply.github.com>
2023-12-20 18:13:58 -0500

    first commit

---
+++ Frakture/module/database.js
@@ -0,0 +1,37 @@
+import mysql from 'mysql2'
+
+import dotenv from 'dotenv'
+dotenv.config()
+
+const pool = mysql.createPool({
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE
+}).promise()
+
+export async function getNotes() {
+    const [rows] = await pool.query("SELECT * FROM notes")
+    return rows
+}
+
+export async fuction getNote(id) {
+    const [rows] = await pool.query('
+    SELECT *
+    FROM notes
+    WHERE id = ?   
+    ', [id])
+    return rows[0]
+}
+
+export async fuction createNote(title, content) {
+    const [result] = await pool.query('
+    INSERT INTO notes (title, content)
+    VALUES (?, ?)
+    ', [title, content])
+    const id = result.insertid
+    return getNote(id)
+}
+
+export const result = await createNote('test', 'test')
+console.log(result)
